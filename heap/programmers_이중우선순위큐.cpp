7941231065263003ffe9dd44c76a453db3b572d7
rzayoon <njy0227@gmail.com>
2021-06-30 19:23:34 +0900

    Update programmers_이중우선순위큐.cpp

--- heap/programmers_이중우선순위큐.cpp
+++ heap/programmers_이중우선순위큐.cpp
@@ -1,30 +1,32 @@
 #include <string>
 #include <vector>
+#include <set>
 using namespace std;
 
-vector<int> _heap;
-int count = 0;
-int min = 0;
-void ins(int i){
-    
-
-}
-
-void del(int i){
-    
-
-}
-
 vector<int> solution(vector<string> operations) {
-    vector<int> answer;
-
+    vector<int> answer(2, 0);
+    multiset<int> heap;
     for(string operation : operations){
         if(operation[0] == 'I'){
             int i = stoi(operation.substr(2));
+            heap.insert(i);
         }
         else if(operation[0] == 'D'){
-            
+            if(heap.empty())
+                continue;
+
+            if(operation[2] == '-'){
+                heap.erase(heap.begin());
+            }
+            else{
+                heap.erase(--heap.end());
+            }
         }
+    }
+
+    if(!heap.empty()){
+        answer[0] = *(--heap.end());
+        answer[1] = *heap.begin();
     }
 
     return answer;
