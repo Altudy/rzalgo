7b01898529dfd0696b483a4e8290050c0dcf4b69
rzayoon <njy0227@gmail.com>
2021-09-24 21:28:37 +0900

    빛의경로사이클
    좀 더 짧게

--- BruteForce/programmers_빛의경로사이클.cpp
+++ BruteForce/programmers_빛의경로사이클.cpp
@@ -10,24 +10,18 @@ int dX[] = { 0, -1, 0, 1 };
 vector<int> solution(vector<string> grid) {
 	vector<int> answer;
 
+	int lenY = grid.size();
+	int lenX = grid[0].length();
 	
-
-	int maxY = grid.size() - 1;
-	int maxX = grid[0].length() - 1;
-	
-
-
-	for (int initY = 0; initY <= maxY; initY++) {
-		for (int initX = 0; initX <= maxX; initX++) {
+	for (int initY = 0; initY < lenY; initY++) {
+		for (int initX = 0; initX < lenX; initX++) {
 			for (int initDir = 0; initDir < 4; initDir++) {
-
+                
 				int nowY(initY), nowX(initX), nowDir(initDir), len(0);
 
 				if (check[nowY][nowX][nowDir]) continue;
 
-
 				while (true) {
-
 					if (check[nowY][nowX][nowDir]) break;
 
 					check[nowY][nowX][nowDir] = true;
@@ -36,37 +30,22 @@ vector<int> solution(vector<string> grid) {
 					int nextX = nowX + dX[nowDir];
 					int nextDir = nowDir;
 
-					
+					nextY = (nextY + lenY) % (lenY);
+                    nextX = (nextX + lenX) % (lenX);
 
-					if (nextY < 0) nextY = maxY;
-					else if (nextY > maxY) nextY = 0;
-					if (nextX < 0) nextX = maxX;
-					else if (nextX > maxX) nextX = 0;
-
-					if (grid[nextY][nextX] == 'R') nextDir = nextDir + 1;
-					if (grid[nextY][nextX] == 'L') nextDir = nextDir - 1;
-
-
-					if (nextDir < 0) nextDir = 3;
-					else  if (nextDir > 3) nextDir = 0;
-
+					if (grid[nextY][nextX] == 'R') nextDir = (nextDir + 1 + 4) % 4;
+					if (grid[nextY][nextX] == 'L') nextDir = (nextDir - 1 + 4) % 4;
+                    
 					nowDir = nextDir;
 					nowX = nextX;
 					nowY = nextY;
 
 					len++;
-
-
-
 				}
-
 				answer.push_back(len);
-
 			}
 		}
 	}
-
-
 	sort(answer.begin(), answer.end());
 	return answer;
 }
